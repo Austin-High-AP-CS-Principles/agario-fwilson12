31f41204f729f8371708ff5fb6a27085004894f9
fwilson12 <145033473+fwilson12@users.noreply.github.com>
2024-01-23 19:29:30 -0600

    fixed

--- agario.py
+++ agario.py
@@ -8,6 +8,16 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("agar.io")
 
+def draw_grid(screen):
+    for x in range(0, screen_width, 50):
+        pygame.draw.line(screen, (230,230,230,230), (x, 0), (x, screen_height))
+
+    for y in range(0, screen_height, 50):
+        pygame.draw.line(screen, (230,230,230,230), (0, y), (screen_width, y))
+
+
+
+
 
 
 clock = pygame.time.Clock()
@@ -53,7 +63,7 @@ class Enemy(pygame.sprite.Sprite):
 
 class Player(Enemy):
     def __init__(self, x, y):
-        super(Enemy, self).__init__()
+        super(Player, self).__init__(x,y)
         self.color = (255, 255, 255)
         self.radius = 55
         self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA, 32)
@@ -65,19 +75,16 @@ class Player(Enemy):
         distx = mx - self.rect.centerx
         disty = my - self.rect.centery
         hyp = math.sqrt(abs(distx**2 - disty**2))
-        if hyp == 0:
-            hyp = 0.01
-        self.deltax = distx/hyp
-        self.deltay = disty/hyp
-        if self.rect.left <= -0 or self.rect.right >= 1400:
-            self.deltax *= -1
-        if self.rect.top <= -0 or self.rect.bottom >= 800:
-            self.deltay *= -1
+        if hyp > 0:
+            self.deltax = distx/hyp 
+            self.deltay = disty/hyp 
         self.rect.centerx += self.deltax
         self.rect.centery += self.deltay
-# players = pygame.sprite.Group()
-# chris = Player(random.randint(600,800),random.randint(450,550))
-# players.add(chris)
+    
+        
+players = pygame.sprite.Group()
+chris = Player(random.randint(600,800),random.randint(450,550))
+players.add(chris)
 
 
 tangoes = pygame.sprite.Group()
@@ -97,12 +104,16 @@ class Food(pygame.sprite.Sprite):
 
 
 meals = pygame.sprite.Group()
-for num in range(20):
-    meals.add(Food("red"))
+for num in range(10):
+    meals.add(Food(random.choice(['red','yellow','green','blue','purple'])))
 objects = pygame.sprite.Group()
 objects.add(meals)
 objects.add(tangoes)
-# objects.add(players)
+objects.add(players)
+
+
+
+
 running = True
 while running:
 
@@ -111,29 +122,36 @@ while running:
             running = False
 
     screen.fill("black")
-
-    # if chris in objects:
-    #     chris.move()
-    # players.draw(screen)
-
+    draw_grid(screen)
+   
+    
+    
     meals.draw(screen)
    
     for tango in tangoes:
         tango.move()
     tangoes.draw(screen)
-   
+    
+    if chris in objects:
+        chris.move()
+    players.draw(screen)
+
     for obj in objects:
             for other in objects:
                 if other != obj and type(obj) != Food:
-                    if math.dist(obj.rect.center, other.rect.center) < (obj.radius + other.radius)*.7 and obj.radius > other.radius:
+                    if math.dist(obj.rect.center, other.rect.center) < (obj.radius + other.radius)*.65 and obj.radius > 1.1 * other.radius:
                         obj.radius += other.radius
                         obj.image = pygame.Surface((obj.radius * 2, obj.radius * 2), pygame.SRCALPHA, 32)
                         obj.image = obj.image.convert_alpha()
                         pygame.draw.circle(obj.image, obj.color, (obj.radius, obj.radius), obj.radius)
-                        objects.remove(other)
-                        other.kill()
+                        obj.rect = obj.image.get_rect(center=obj.rect.center)
+                        if type(other) != Food:
+                            objects.remove(other)
+                            other.kill()
+                        elif type(other) == Food:
+                            other.rect.x, other.rect.y = (random.randint(50,1350),random.randint(30, 770))
 
-
+    
     pygame.display.flip()
     clock.tick(60)
 
